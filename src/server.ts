d2977512c29c414763399ba15fa355cf437ff576
Georgi Kostadinov Georgiev <georgi.ko.georgiev@a1.bg>
2026-02-27 14:33:05 +0200

    Created scaffold. Implemented TS - 27.02.26

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+// Connect database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
