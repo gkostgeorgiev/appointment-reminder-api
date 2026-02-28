8b09a579a694083a082cf8492db1f8c90b73eb1c
gkostgeorgiev <112526622+gkostgeorgiev@users.noreply.github.com>
2026-02-28 18:34:14 +0200

    Updated email validation through zod in Customer schema - 28.02.26

--- src/validators/customerSchemas.ts
+++ src/validators/customerSchemas.ts
@@ -6,7 +6,7 @@ export const updateCustomerSchema = z.object({
       firstName: z.string().trim().min(1).optional(),
       lastName: z.string().trim().min(1).optional(),
       phone: z.string().trim().min(1).optional(),
-      email: z.string().trim().pipe(z.string().email()).optional(),
+      email: z.string().trim().pipe(z.email()).optional(),
     })
     .refine((data) => Object.keys(data).length > 0, {
       message: "At least one field must be provided",
